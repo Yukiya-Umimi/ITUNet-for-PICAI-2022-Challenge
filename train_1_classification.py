fdd3b08d2da40949ffcce11910c0d08236dc3f28
Joeran Bosma <joeran@bosma.co>
2023-02-26 16:42:38 +0100

    Training scripts

---
+++ train_1_classification.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import shutil
+from pathlib import Path
+from subprocess import check_call
+
+
+def main(taskname="Task2203_picai_baseline"):
+    """Training method from Swangeese team for PI-CAI Challenge."""
+    parser = argparse.ArgumentParser()
+
+    # input data and model directories
+    parser.add_argument('--workdir', type=str, default="/workdir")
+    parser.add_argument('--imagesdir', type=str, default=os.environ.get('SM_CHANNEL_IMAGES', "/input/images"))
+    parser.add_argument('--labelsdir', type=str, default=os.environ.get('SM_CHANNEL_LABELS', "/input/picai_labels"))
+    parser.add_argument('--preprocesseddir', type=str, default=os.environ.get('SM_CHANNEL_PREPROCESSED', "/input/preprocessed"))
+    parser.add_argument('--outputdir', type=str, default=os.environ.get('SM_MODEL_DIR', "/output"))
+    parser.add_argument('--checkpointsdir', type=str, default="/checkpoints")
+    parser.add_argument('--folds', type=int, nargs="+", default=(0, 1, 2, 3, 4),
+                        help="Folds to train. Default: 0 1 2 3 4")
+
+    args, _ = parser.parse_known_args()
+
+    # paths
+    workdir = Path(args.workdir)
+    images_dir = Path(args.imagesdir)
+    labels_dir = Path(args.labelsdir)
+    output_dir = Path(args.outputdir)
+    checkpoints_dir = Path(args.checkpointsdir)
+    preprocessed_dir = Path(args.preprocesseddir)
+
+    workdir.mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # descibe input data
+    print(f"workdir: {workdir}")
+    print(f"images_dir: {images_dir}")
+    print(f"labels_dir: {labels_dir}")
+    print(f"output_dir: {output_dir}")
+
+    print("Images folder:", os.listdir(images_dir))
+    print("Labels folder:", os.listdir(labels_dir))
+
+    # Train classification model
+    # TODO: add training commands for classification model
+
+    # Export trained models
+    # TODO: add commands for exporting trained models
+
+
+if __name__ == '__main__':
+    main()
