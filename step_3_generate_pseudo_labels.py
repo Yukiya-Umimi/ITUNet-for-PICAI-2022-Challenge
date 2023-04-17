f1f3a57b828584734f5fbafaa11725869851ea24
Joeran Bosma <joeran@bosma.co>
2023-04-17 16:37:03 +0200

    Update step_3_generate_pseudo_labels.py

--- step_3_generate_pseudo_labels.py
+++ step_3_generate_pseudo_labels.py
@@ -2,8 +2,10 @@ import argparse
 import os
 from pathlib import Path
 
+import numpy as np
+
 from classification.cls_data import predict_test5c
-from segmentation.predict_2d import postprecess, save_npy, vote_dir
+from segmentation.predict_2d import postprecess, predict_process, vote_dir, Config
 
 
 def main(taskname="Task2203_picai_baseline"):
@@ -15,6 +17,7 @@ def main(taskname="Task2203_picai_baseline"):
     parser.add_argument('--imagesdir', type=str, default=os.environ.get('SM_CHANNEL_IMAGES', "/input/images"))
     parser.add_argument('--labelsdir', type=str, default=os.environ.get('SM_CHANNEL_LABELS', "/input/picai_labels"))
     parser.add_argument('--preprocesseddir', type=str, default=os.environ.get('SM_CHANNEL_PREPROCESSED', "/input/preprocessed"))
+    parser.add_argument('--supervisedweightsdir', type=str, default=os.environ.get('SM_CHANNEL_SUPERVISED_WEIGHTS', "/input/weights"))
     parser.add_argument('--outputdir', type=str, default=os.environ.get('SM_MODEL_DIR', "/output"))
     parser.add_argument('--checkpointsdir', type=str, default="/checkpoints")
     parser.add_argument('--folds', type=int, nargs="+", default=(0, 1, 2, 3, 4),
@@ -29,6 +32,7 @@ def main(taskname="Task2203_picai_baseline"):
     output_dir = Path(args.outputdir)
     checkpoints_dir = Path(args.checkpointsdir)
     preprocessed_dir = Path(args.preprocesseddir)
+    supervised_weights_dir = Path(args.supervisedweights)
 
     workdir.mkdir(parents=True, exist_ok=True)
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -41,20 +45,20 @@ def main(taskname="Task2203_picai_baseline"):
 
     # Perform inference with the classification model
     predict_test5c(
-        weight_path: str = '/opt/cls_algorithm/weights/',
-        base_dir: str = 'path/to/nnUNet_test_data',
-        csv_save_path: str = 'test_3c.csv',
+        weight_path=supervised_weights_dir / 'ckpt/picai/v0/',
+        base_dir=preprocessed_dir / 'nnUNet_test_data',
+        csv_save_path=workdir / 'test_3c.csv',
     )
 
     # Perform inference with the segmentation model
     data_path = preprocessed_dir / "nnUNet_test_data"
-    outdir = './segout/segmentation_result'
+    outdir = workdir / 'segout/segmentation_result'
     config = Config()
     for fold in range(1,6):
         print('****fold%d****'%fold)
         config.fold = fold
-        config.ckpt_path = f'./new_ckpt/seg/{config.version}/fold{str(fold)}'
-        save_dir = f'./segout/{config.version}/fold{str(fold)}'
+        config.ckpt_path = supervised_weights_dir / f'ckpt/seg/{config.version}/fold{str(fold)}'
+        save_dir = workdir / f'segout/{config.version}/fold{str(fold)}'
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         pathlist = ['_'.join(path.split('_')[:2]) for path in os.listdir(data_path)]
@@ -64,8 +68,15 @@ def main(taskname="Task2203_picai_baseline"):
             pred = predict_process(path,config,data_path)
             print(pred.shape)
             np.save(os.path.join(save_dir,path+'.npy'),pred)
-    vote_dir()
-    postprecess(outdir)
+
+    # Ensemble segmentation results
+    vote_dir(datadir=workdir / f'segout/{config.version}')
+
+    # Postprocess segmentation results
+    postprecess(
+        data_dir=workdir / f'segout/{config.version}/avg',
+        outdir=outdir,
+    )
 
 
     # Generate pseudo labels
